9a067f6bd6d61c7b03662a828705969307fda170
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-04-24 14:56:32 +0700

    fix authention

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import {Redirect, Route, Routes, useNavigate} from "react-router-dom";
 import HomePage from "./components/UI/HomePage";
 import Logout from "./components/UI/Logout";
 import Login from "./components/UI/Login";
@@ -12,26 +12,41 @@ import WalletDetail from "./components/UI/Wallet/WalletDetail";
 import AccountModal from "./components/UI/Modal/AccountModal";
 import ChangePassword from "./components/UI/Modal/ChangePasswordModal";
 import AddTransactionForm from "./components/UI/Dashboard/AddTransaction/AddTransactionForm";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
+
+    const auth = useSelector(state => state.auth)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!auth.isLoggedIn) {
+            navigate('/')
+        }
+    }, [auth.isLoggedIn])
+
+
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}></Route>
-            <Route path="/auth/logout" element={<Logout/>}></Route>
             <Route path="/auth/login" element={<Login/>}></Route>
             <Route path="/auth/register" element={<Register/>}></Route>
 
-
-            <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
-            <Route path="/update-profile/:id" element={<UpdateProfile/>}></Route>
-            <Route path="/dashboard" element={<Dashboard/>}></Route>
-            <Route path="/my-wallet" element={<MyWallet/>}></Route>
-            <Route path="/wallet-detail/:id" element={<WalletDetail/>}></Route>
-            <Route path="/account/:id" element={<AccountModal/>}></Route>
-            <Route path="/change-password" element={<ChangePassword/>}></Route>
-            <Route path="/transaction" element={<AddTransactionForm/>}></Route>
-            <Route path="/account/:id" element={<AccountModal/>}></Route>
-
+                { auth.isLoggedIn && (
+                    <>
+                            <Route path="/auth/logout" element={<Logout/>}></Route>
+                            <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+                            <Route path="/update-profile/:id" element={<UpdateProfile/>}></Route>
+                            <Route path="/dashboard" element={<Dashboard/>}></Route>
+                            <Route path="/my-wallet" element={<MyWallet/>}></Route>
+                            <Route path="/wallet-detail/:id" element={<WalletDetail/>}></Route>
+                            <Route path="/account/:id" element={<AccountModal/>}></Route>
+                            <Route path="/change-password" element={<ChangePassword/>}></Route>
+                            <Route path="/transaction" element={<AddTransactionForm/>}></Route>
+                            <Route path="/account/:id" element={<AccountModal/>}></Route>
+                    </>
+                )  }
 
         </Routes>
     );
--- src/components/UI/Dashboard/Navbar.js
+++ src/components/UI/Dashboard/Navbar.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
     Navbar,
     Typography,
-    Button,
     IconButton,
 } from "@material-tailwind/react";
 import logo from '../../img/ic_category_all.png';
@@ -11,9 +10,9 @@ import AddTransactionModal from "./AddTransaction/AddTransactionModal";
 
 
 export default function NavBar() {
-    const [openNav, setOpenNav] = React.useState(false);
+    const [openNav, setOpenNav] = useState(false);
 
-    React.useEffect(() => {
+    useEffect(() => {
         window.addEventListener(
             "resize",
             () => window.innerWidth >= 960 && setOpenNav(false)
@@ -23,16 +22,16 @@ export default function NavBar() {
     const navList = (
         <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <svg href="#"
-                 class="h-8 w-8 text-gray-500"
-                 width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 className="h-8 w-8 text-gray-500"
+                 width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="4" y="5" width="16" height="16" rx="2" />  <line x1="16" y1="3" x2="16" y2="7" />  <line x1="8" y1="3" x2="8" y2="7" />  <line x1="4" y1="11" x2="20" y2="11" />  <line x1="11" y1="15" x2="12" y2="15" />  <line x1="12" y1="15" x2="12" y2="18" /></svg>
 
             <svg href="#"
-                 class="h-8 w-8 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="2" />  <path d="M2 12l1.5 2a11 11 0 0 0 17 0l1.5 -2" />  <path d="M2 12l1.5 -2a11 11 0 0 1 17 0l1.5 2" /></svg>
+                 className="h-8 w-8 text-gray-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="2" />  <path d="M2 12l1.5 2a11 11 0 0 0 17 0l1.5 -2" />  <path d="M2 12l1.5 -2a11 11 0 0 1 17 0l1.5 2" /></svg>
 
             <svg href="#"
-                 class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                 className="h-8 w-8 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
             </svg>
 
 
@@ -64,13 +63,6 @@ export default function NavBar() {
                     </div>
                     <div className="flex items-center gap-4">
                         <div className="mr-4 hidden lg:block">{navList}</div>
-                        {/*<Button*/}
-                        {/*    variant="gradient"*/}
-                        {/*    size="sm"*/}
-                        {/*    className="hidden lg:inline-block bg-green-500"*/}
-                        {/*>*/}
-                        {/*    <span>ADD TRANSACTION</span>*/}
-                        {/*</Button>*/}
                         <AddTransactionModal/>
                         <IconButton
                             variant="text"
