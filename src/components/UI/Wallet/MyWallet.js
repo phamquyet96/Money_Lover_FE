f220b85193afece99dfa2994018dd9708b24d53a
m8nh <86234265+m8nh@users.noreply.github.com>
2023-04-25 14:44:37 +0700

    fix create wallet

--- src/components/UI/Wallet/MyWallet.js
+++ src/components/UI/Wallet/MyWallet.js
@@ -3,12 +3,19 @@ import WalletHeader from "./WalletHeader";
 import MyWalletDetail from "./MyWalletDetail";
 
 const MyWallet = () => {
+
+    const [createWalletStatus, setCreateWalletStatus] = React.useState(false);
+
+    const sendStatusCreateWallet = (e) => {
+        setCreateWalletStatus(e);
+    }
+
     return (
         <>
             <div className="min-h-screen flex flex-col">
                 <div className="bg-custom-gray flex-1">
-                    <WalletHeader/>
-                    <MyWalletDetail/>
+                    <WalletHeader status={sendStatusCreateWallet}/>
+                    <MyWalletDetail statusFinal={createWalletStatus}/>
                 </div>
             </div>
         </>
